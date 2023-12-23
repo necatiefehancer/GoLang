15dd9e891911cc9a3b4b3d578099819c182fc350
Necati Efe Hançer <130218089+necatiefehancer@users.noreply.github.com>
2023-12-23 14:16:37 +0300

    [][GO][EFE]Variables passed

--- main.go
+++ main.go
@@ -1,28 +1,79 @@
 package main
 
-import (
-	"fmt"
-	"os"
+import "fmt"
+
+const text string = "efe"
+
+var (
+	degisken1 string = "efe"
+	degisken2 int    = 23
 )
 
 func main() {
 
-	// fmt.Println("hello planet")
-	// fmt.Println("www.heraSystem.com")
+	fmt.Println(degisken1, degisken2)
+	fmt.Println(text)
+	fmt.Println("hello planet")
+	fmt.Println("www.heraSystem.com")
 
-	// for _, env := range os.Environ() {
-	// 	fmt.Println(env)
-	// }
+	for _, env := range os.Environ() {
+		fmt.Println(env)
+	}
 
 	var uName string = os.Getenv("USERNAME")
 	var uDomain string = os.Getenv("USERDOMAIN")
 	var processorArthitecture string = os.Getenv("PROCESSOR_ARCHITECTURE")
 	var processorIdentifier string = os.Getenv("PROCESSOR_IDENTIFIER")
 	var processorLevel string = os.Getenv("PROCESSOR_LEVEL")
+	var goRoot string = os.Getenv("GOROOT")
+	var goPath string = os.Getenv("GOPATH")
+	var homePath string = os.Getenv("HOMEPATH")
 
-	fmt.Println(uName)
-	fmt.Println(uDomain)
-	fmt.Println(processorArthitecture)
+	fmt.Println("username" + uName)
+	fmt.Println("Domain" + uDomain)
+	fmt.Println("İşlemci Mimarisi " + processorArthitecture)
 	fmt.Println(processorIdentifier)
-	fmt.Println(processorLevel)
+	fmt.Println("İşlemci seviyesi " + processorLevel)
+	fmt.Println("GO ROOT ALANI" + goRoot)
+	fmt.Println("GO PATH ALANI" + goPath)
+	fmt.Println("HOME PATH ALANI" + homePath)
+
+	public private kapsamı kapısına çıkan isimlendirmeler
+
+	var projectName string = "GoBackendServer" //dış dosyalardan buna erişilmesin private yapısı
+	var ProjectName string = "GOBackendServer" // dış dosyalardan buna erişilsin public yapısı
+
+	var name string
+	name = "necatiefe"
+	fmt.Println(name)
+
+	var a, b, c int
+	var a, b, c int = 3, 4, 5
+
+	fmt.Println(a, b, c)
+
+	var message = "HelloPlanet"
+	var a, b, c = 3, true, 4.8
+	fmt.Println(message, a, b, c)
+
+	var k , o string ="abc","xyz"
+
+	var p = 42
+	var s,b="xyz",true
+
+	n:=28
+	name, age := "necati efe", 20
+	fmt.Println(name, age)
+
+	a := "Text"
+	b := 'T'
+	c := `Text`
+
+	fmt.Println(a, b, c)
+
+	var myFloat32 float32 = 58.
+	myComplex := complex(5, 6)
+	println(myFloat32)
+	println(myComplex)
+
 }
