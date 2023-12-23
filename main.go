94d19188a9ac74b64cac66b0cc692a2df7f2ffdc
Necati Efe Hançer <130218089+necatiefehancer@users.noreply.github.com>
2023-12-23 13:05:31 +0300

    [][GO][EFE]For go push

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("hello planet")
+
+	// fmt.Println("hello planet")
+	// fmt.Println("www.heraSystem.com")
+
+	// for _, env := range os.Environ() {
+	// 	fmt.Println(env)
+	// }
+
+	var uName string = os.Getenv("USERNAME")
+	var uDomain string = os.Getenv("USERDOMAIN")
+	var processorArthitecture string = os.Getenv("PROCESSOR_ARCHITECTURE")
+	var processorIdentifier string = os.Getenv("PROCESSOR_IDENTIFIER")
+	var processorLevel string = os.Getenv("PROCESSOR_LEVEL")
+
+	fmt.Println(uName)
+	fmt.Println(uDomain)
+	fmt.Println(processorArthitecture)
+	fmt.Println(processorIdentifier)
+	fmt.Println(processorLevel)
 }
